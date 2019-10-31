5a07313b761f237e43ca7c378bd514c2cce55e85
LaoHeILY <1037150348@qq.com>
2019-10-31 13:25:35 +0800

    react

---
+++ react-app/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+
+function App() {
+  return (
+   <div className="App">
+     <Header></Header>
+     iajfiek
+     nkrjaf
+     <p>hahah</p>
+     <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
